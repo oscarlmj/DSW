e682f4d821570b7089ebd29d99b3266449668e32
Oscar Moreno <oscarlmorenojorge@gmail.com>
2023-11-23 08:09:00 +0000

    Actividad periodicos

--- UT4/periodicos.php
+++ UT4/periodicos.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 $periodicos = [
     "elpais" => "https://elpais.com",
     "elmundo" => "https://elmundo.es",
@@ -9,43 +8,33 @@ $periodicos = [
     "elperiodico" => "https://elperiodico.com"
 ];
 
-foreach ($periodicos as $clave => $url) {
-    // Establece un contador para cada periódico en la sesión
-    if (!isset($_SESSION[$clave])) {
-        $_SESSION[$clave] = 0;
-    }
+foreach ($periodicos as $periodico) {
+    // El valor de la cookie puede ser, por ejemplo, la fecha actual
+    $valorCookie =0;
+    $nombre=explode("//",$periodico);
+    $nombreCookie = str_replace('_', '.', $nombre);
+
+    // Establecemos la cookie con un tiempo de expiración de una hora (3600 segundos)
+    setcookie($nombreCookie, $valorCookie, time() + 3600, '/');
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Verifica si se ha enviado un formulario
-    foreach ($periodicos as $clave => $url) {
-        if (isset($_POST[$clave])) {
-            // Aumenta el contador correspondiente en la sesión
-            $_SESSION[$clave]++;
-            header('Location: ' . $url); // Redirige al enlace del periódico
-            exit();
-        }
-    }
-}
+print_r($_COOKIE);
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Periódicos</title>
+    <title>Periodicos</title>
 </head>
 <body>
-    <form method="post">
-        <?php foreach ($periodicos as $clave => $url): ?>
-            <?php $nombre = explode("//", $url) ?>
-            <button type="submit" name="<?php echo $clave; ?>"><?php echo $nombre[1]; ?></button>
-            <span>Accedido: <?php echo $_SESSION[$clave]; ?> veces</span>
-            <br>
-        <?php endforeach; ?>
-    </form>
+    <?php foreach ($periodicos as $periodico): ?>
+        <?php $nombre= explode("//",$periodico)?>
+        <a href="<?php echo $periodico; ?>"><?php echo $nombre[1];?></a>
+        <span>Accedido: <?php $_COOKIE[$nombre[1]]?></span>
+        <br>
+    <?php endforeach; ?>
 </body>
 </html>
-
 
