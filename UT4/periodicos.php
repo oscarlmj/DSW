860855ff4f789c0f339168d2374869a36290747c
oscarlmj <oscarlmorenojorge@gmail.com>
2023-12-04 19:18:49 +0000

    Repaso cookies sesion

--- UT4/periodicos.php
+++ UT4/periodicos.php
@@ -1,4 +1,5 @@
 <?php
+// Inicia la sesión para manejar cookies
 session_start();
 
 $periodicos = [
@@ -10,9 +11,9 @@ $periodicos = [
 ];
 
 foreach ($periodicos as $clave => $url) {
-    // Establece un contador para cada periódico en la sesión
-    if (!isset($_SESSION[$clave])) {
-        $_SESSION[$clave] = 0;
+    // Establece un contador para cada periódico en las cookies
+    if (!isset($_COOKIE[$clave])) {
+        setcookie($clave, 0, time() + 3600); // Expira en 1 hora
     }
 }
 
@@ -20,8 +21,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Verifica si se ha enviado un formulario
     foreach ($periodicos as $clave => $url) {
         if (isset($_POST[$clave])) {
-            // Aumenta el contador correspondiente en la sesión
-            $_SESSION[$clave]++;
+            // Aumenta el contador correspondiente en las cookies
+            $contador = $_COOKIE[$clave] + 1;
+            setcookie($clave, $contador, time() + 3600); // Actualiza la cookie con el nuevo valor
             header('Location: ' . $url); // Redirige al enlace del periódico
             exit();
         }
@@ -41,11 +43,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <?php foreach ($periodicos as $clave => $url): ?>
             <?php $nombre = explode("//", $url) ?>
             <button type="submit" name="<?php echo $clave; ?>"><?php echo $nombre[1]; ?></button>
-            <span>Accedido: <?php echo $_SESSION[$clave]; ?> veces</span>
+            <span>Accedido: <?php echo isset($_COOKIE[$clave]) ? $_COOKIE[$clave] : 0; ?> veces</span>
             <br>
         <?php endforeach; ?>
     </form>
 </body>
 </html>
-
-
