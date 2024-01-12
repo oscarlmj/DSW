5d64ca2dbb430b92d3d1e36947e06b972a8154a3
oscarlmj <oscarlmorenojorge@gmail.com>
2024-01-12 20:16:31 +0000

    Comentarios y correccion de pequeños errores

--- UT5/Actividad/objetos.php
+++ UT5/Actividad/objetos.php
@@ -1,78 +1,96 @@
 <?php
 
+//Array donde se almacenarán los objetos una vez creados.
 $viviendas = [];
 
-
+//Clase abstracta.
 abstract class Vivienda {
+    //Atributos de la clase.
     protected $num_habitaciones;
     protected $num_baños;
     protected $num_plantas;
     protected $color;
 
-
+    //Constructor de la clase con 3 parametros.
     public function __construct( int $num_habitaciones, int $num_baños, string $color ) {
         $this->num_habitaciones = $num_habitaciones;
         $this->num_baños = $num_baños;
         $this->color = $color;
     }
 
+    //Metodo que devuelve el numero de habitaciones.
     public function get_num_habitaciones() {
         return $this->num_habitaciones;
     }
 
+    //Metodo que devuelve el numero de baños.
     public function get_num_baños() {
         return $this->num_baños;
     }
 
+    //Metodo abstracto que devuelve el numero de plantas.
     abstract public function get_num_plantas();
 
+    //Metodo que devuelve el color de la casa.
     public function get_color(){
         return $this->color;
     }
 }
 
+//Interfaz mediante la cual se le cambia el color a las casas.
 interface cambiarColor{
     public function pintarCasa();
 }
 
+//Objeto creado a partir de la clase abstracta, donde ademas se implementa la interfaz.
+//En este caso, el metodo abstracto genera un numero aleatorio entre 2 y 3 para conocer el numero de plantas.
 class Duplex extends Vivienda implements CambiarColor {
     protected $num_plantas;
 
+    //Metodo constructor.
     public function __construct(int $num_habitaciones, int $num_baños, int $num_plantas, string $color) {
         parent::__construct($num_habitaciones, $num_baños, $color);
         $this->num_plantas = $num_plantas;
     }
 
+    //Implementacion de la interfaz. Se genera un numero aleatorio de 0 a 9, para elegir el color en el array.
     public function pintarCasa()
     {
         $colores = ["Rojo", "Verde", "Azul", "Amarillo", "Naranja", "Morado", "Rosa", "Gris", "Negro", "Blanco"];
         $this->color = $colores[$numeroAleatorio = rand(0, 9)];
     }
 
+    //Metodo abstracto de la clase Vivienda.
     public function get_num_plantas()
     {
+        $numeroPlantas = rand(2,3);
         return $this->num_plantas;
     }
 }
 
-
+//Objeto Piso creado a partir de la clase abstracta Vivienda.
 class Piso extends Vivienda {
 
+    //Se establece por defecto el numero de baños a 1.
     protected $num_baños=1;
 
+    //Metodo constructor.
     public function __construct(int $num_habitaciones, int $num_baños, string $color) {
         parent::__construct($num_habitaciones, $num_baños, $color);
     }
 
+    //Metodo de la clase abstracta, pero en este caso, devuelve siempre 1.
     public function get_num_plantas()
     {
         return 1;
     }
 }
 
+//Metodo mediante el cual se crear aleatoriamente objetos de los ya definidos, y se almacenan en el array viviendas.
 function crearUrbanizacion()
 {
     global $viviendas;
+
     for ($i = 0; $i <= 10; $i++) {
         $numeroAleatorio = rand(1,2);
         if ($numeroAleatorio == 1) {
@@ -84,6 +102,7 @@ function crearUrbanizacion()
     }
 }
 
+//Funcion que muestra por pantalla un mensaje descriptivo de cada objeto creado.
 function listarviviendas()
 {
     global $viviendas;
@@ -100,13 +119,12 @@ function listarviviendas()
     }
 }
 
+//Ejecución.
 crearUrbanizacion();
 $duplex  =new Duplex(2,2,2,"Amarillo");
 array_push($viviendas,$duplex);
 $duplex->pintarCasa();
 listarviviendas();
-
-
 ?>
 
 
